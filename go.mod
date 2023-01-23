65814c0b7278cab1749c2bd0051c638f0392a269
govardhan <inboxgovardhan@gmail.com>
2023-01-23 22:11:27 +0530

    added g osum

---
+++ go.mod
@@ -0,0 +1,7 @@
+module aws-secret-manager
+
+go 1.19
+
+require github.com/aws/aws-sdk-go v1.44.184
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- main.go
+++ main.go
@@ -8,38 +8,60 @@ import (
 )
 
 func main() {
-
-	sess, err := session.NewSession(&aws.Config{Region: aws.String("us-east-1")})
-	if err != nil {
-		fmt.Println(err)
-		return
+	regions := []string{
+		"us-east-1",
+		"us-east-2",
+		"us-west-1",
+		"us-west-2",
+		"ap-northeast-1",
+		"ap-northeast-2",
+		"ap-northeast-3",
+		"ap-south-1",
+		"ap-southeast-1",
+		"ap-southeast-2",
+		"ca-central-1",
+		"eu-central-1",
+		"eu-west-1",
+		"eu-west-2",
+		"eu-west-3",
+		"eu-north-1",
+		"sa-east-1",
 	}
 
-	svc := secretsmanager.New(sess)
+	for i := range regions {
 
-	max := int64(100)
-	listinput := &secretsmanager.ListSecretsInput{
-		MaxResults: &max,
-	}
-
-	result, err := svc.ListSecrets(listinput)
-	if err != nil {
-	
-		fmt.Printf("error inlisting secrets: %+v", err.Error())
-		return
-	}
-	delete := true
-	for i := range result.SecretList {
-		input := &secretsmanager.DeleteSecretInput{
-			SecretId:                   result.SecretList[i].ARN,
-			ForceDeleteWithoutRecovery: &delete,
+		sess, err := session.NewSession(&aws.Config{Region: aws.String(regions[i])})
+		if err != nil {
+			fmt.Println(err)
+			return
 		}
 
-		_, err := svc.DeleteSecret(input)
+		svc := secretsmanager.New(sess)
+
+		max := int64(100)
+		listinput := &secretsmanager.ListSecretsInput{
+			MaxResults: &max,
+		}
+
+		result, err := svc.ListSecrets(listinput)
 		if err != nil {
-			
-			fmt.Printf("error in deleting secret: %+v", err.Error())
+
+			fmt.Printf("error inlisting secrets: %+v", err.Error())
 			return
+		}
+		delete := true
+		for i := range result.SecretList {
+			input := &secretsmanager.DeleteSecretInput{
+				SecretId:                   result.SecretList[i].ARN,
+				ForceDeleteWithoutRecovery: &delete,
+			}
+
+			_, err := svc.DeleteSecret(input)
+			if err != nil {
+
+				fmt.Printf("error in deleting secret: %+v", err.Error())
+				return
+			}
 		}
 	}
 
